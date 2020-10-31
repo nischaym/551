ac38790bed697640441d0311260a2f1e30db6646
Nischay Maridevegowda <nischay@Nischays-MacBook-Pro.local>
2020-10-31 13:34:18 -0400

    adding db connection file

--- DataAccess/DADemographicInformation.cs
+++ DataAccess/DADemographicInformation.cs
@@ -1,8 +1,47 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DataAccess
 {
-    public class Class1
+    public class DADemographicInformation
     {
+
+        public static DataSet GetDemographicsUsingWithConfig()
+        {
+            DataTable dt = new DataTable();
+            DataSet demographicInfomation = new DataSet();
+
+            SqlConnection cnn;
+            var cs = ConfigurationManager.ConnectionStrings["DemographicsConnection_DEV"].ConnectionString;
+            cnn = new SqlConnection(cs);
+
+            cnn.Open();
+            string sql = "Select * from InduvidualDemographics";
+
+            SqlCommand command;
+
+            command = new SqlCommand(sql, cnn);
+
+            SqlDataReader dataReader;
+            dataReader = command.ExecuteReader();
+
+
+            dt.Load(dataReader);
+
+            demographicInfomation.Tables.Add(dt);
+
+
+            dataReader.Close();
+            command.Dispose();
+
+            cnn.Close();
+
+            return demographicInfomation;
+
+        }
+
+
     }
 }
