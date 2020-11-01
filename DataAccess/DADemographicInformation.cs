949aabc34bea4ac014ce8665a1848c5af93b9919
Nischay Maridevegowda <nischay@Nischays-MacBook-Pro.local>
2020-11-01 00:54:04 -0400

    adding w/o db method

--- DataAccess/DADemographicInformation.cs
+++ DataAccess/DADemographicInformation.cs
@@ -8,6 +8,34 @@ namespace DataAccess
     public class DADemographicInformation
     {
 
+        public static DataSet GetDemographicsUsingWithOutDB()
+        {
+            DataSet demographicInfomation = new DataSet();
+
+            demographicInfomation.DataSetName = "Demographics";
+            DataTable dt = new DataTable("Basic Information");
+
+            dt.Clear();
+
+            dt.Columns.Add(new DataColumn("FirstName", typeof(string)));
+            dt.Columns.Add(new DataColumn("LastName", typeof(string)));
+            dt.Columns.Add(new DataColumn("Age", typeof(int)));
+            dt.Columns.Add(new DataColumn("Address", typeof(string)));
+
+            DataRow dr = dt.NewRow();
+            dr["FirstName"] = "John";
+            dr["LastName"] = "Doe";
+            dr["Age"] = 45;
+            dr["Address"] = "1 station Landing";
+
+            dt.Rows.Add(dr);
+
+            demographicInfomation.Tables.Add(dt);
+
+            return demographicInfomation;
+
+        }
+
         public static DataSet GetDemographicsUsingWithConfig()
         {
             DataTable dt = new DataTable();
@@ -41,7 +69,6 @@ namespace DataAccess
             return demographicInfomation;
 
         }
-
 
     }
 }
